db3b8af8d5ce462c3dfae9efb48a1edbc837584f
Ywanc <liyangkhoo@gmail.com>
2025-08-25 09:00:24 +0800

    cleaned up UI code

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 from infer import *
 
+def show_case(case, sim_score):
+    #link = case['download link']
+    case_facts = case['elements']['Legal Facts']
+    if case_facts.startswith("Please"):
+        case_facts = "Not found"
+    with st.expander(f"**{case['case_id']} | {case['title']}**"):
+        st.markdown(
+            f"<div style='font-size: 14px; font-family: 'Source Sans Pro', sans-serif;'>"
+            f"<b>Facts Summary:</b><br> {case_facts}<br><br>"
+            f"<a href='{case['download link']}' target='_blank'>Download PDF</a>"
+            f"</div>",
+            unsafe_allow_html=True
+        )
+        st.markdown(f"Similarity score: {sim_score:.4f}")
+    
+    
+    
 st.title("AI Case Search")
 
 description = """
@@ -14,7 +31,7 @@ how_to_use = """
 
 **Step 2**: Enter your Facts/Issues to search for similar case, press the Search button.
 
-**Step 3**: Hopefully the system will return top 5 cases that are most similar to your Facts/Issues.
+**Step 3**: The system will return top 5 cases that are most similar to your Facts/Issues.
 """
 
 
@@ -33,19 +50,9 @@ if search_mode == "**Facts**":
         if query:
             with st.spinner("Searching ... "):
                 results = search_facts(query, k=5) # list of (file, sim_score)
-                
+    
             for case, sim_score in results:
-                link = case['download link']
-                with st.expander(f"**{case['case_id']} | {case['title']}**"):
-                    st.markdown(
-                        f"<div style='font-size: 14px; font-family: 'Source Sans Pro', sans-serif;'>"
-                        f"<b>Facts Summary:</b><br> {case['elements']['Legal Facts']}<br><br>"
-                        f"<b>Issue Summary:</b><br> {case['elements']['Legal Issues']}<br><br>"
-                        f"<a href='{case['download link']}' target='_blank'>Download PDF</a>"
-                        f"</div>",
-                        unsafe_allow_html=True
-                    )
-                    st.markdown(f"Similarity score: {sim_score:.4f}")
+                show_case(case, sim_score)
 
         else:
             st.warning("Please enter your case facts.")
@@ -59,17 +66,6 @@ elif search_mode == "**Issues**":
                 results = search_issues(query, k=5) # list of (file, sim_score)
                 
             for case, sim_score in results:
-                link = case['download link']
-                with st.expander(f"**{case['case_id']} | {case['title']}**"):
-                    st.markdown(
-                        f"<div style='font-size: 14px; font-family: 'Source Sans Pro', sans-serif;'>"
-                        f"<b>Facts Summary:</b><br> {case['elements']['Legal Facts']}<br><br>"
-                        f"<b>Issue Summary:</b><br> {case['elements']['Legal Issues']}<br><br>"
-                        f"<a href='{case['download link']}' target='_blank'>Download PDF</a>"
-                        f"</div>",
-                        unsafe_allow_html=True
-                    )
-                    st.markdown(f"Similarity score: {sim_score:.4f}")
-
+                show_case(case, sim_score)
         else:
             st.warning("Please enter your case Issue.")
